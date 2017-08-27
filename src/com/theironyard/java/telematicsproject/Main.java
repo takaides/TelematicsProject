33bf22b62fc8e876442e19437bc1a13f18b3d6ac
takaides <takaides+github@gmail.com>
2017-08-26 21:46:21 -0400

    starter files

---
+++ src/com/theironyard/java/telematicsproject/Main.java
@@ -0,0 +1,8 @@
+package com.theironyard.java.telematicsproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/theironyard/java/telematicsproject/VehicleInfo.java
@@ -0,0 +1,4 @@
+package com.theironyard.java.telematicsproject;
+
+public class VehicleInfo {
+}
